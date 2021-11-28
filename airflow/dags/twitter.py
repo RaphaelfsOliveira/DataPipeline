0aee1fbbf964259a0d09e52715c7f474e8ea9b01
Raphael Oliveira <raphael.oliveira@sensedata.com.br>
2021-11-28 11:20:00 -0300

    SparkJob

--- airflow/dags/twitter.py
+++ airflow/dags/twitter.py
@@ -2,11 +2,13 @@ from airflow import DAG
 from datetime import datetime
 from os.path import join
 from operators.twitter_operator import TwitterOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 
 start_date = datetime.now()
 
 with DAG(dag_id='twitter', start_date=start_date) as dag:
+    
     twitter_operator = TwitterOperator(
         task_id="twitter_alura_online",
         query="AluraOnline",
@@ -16,4 +18,15 @@ with DAG(dag_id='twitter', start_date=start_date) as dag:
             'extract_date={{ ds }}',
             'alura_online_{{ ds_nodash }}.json'
         )
+    )
+
+    twitter_transform = SparkSubmitOperator(
+        task_id="transform_twitter_alura_online",
+        application="/Users/raphael/Galpao/data_engineer/data_pipeline/spark/transformation.py",
+        name="twitter_transformation",
+        application_args=[
+            "--source", 
+            "--path-dest",
+            "--extract-date",
+        ]
     )
